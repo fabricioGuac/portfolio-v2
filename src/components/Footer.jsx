1fe942065e759e29c43736bef0ec99b56dbf0c9c
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-07-16 23:40:08 -0700

    installs react-icons package and refactors the footer and project components to leverage it

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
+import { FaGithub, FaLinkedin, FaStackOverflow  } from "react-icons/fa"
+
 export default function Footer() {
     return (
-        <footer className="d-flex justify-content-center">
-            <h3>I AM THE FOOTER!!</h3>
-        </footer>
+<footer className="d-flex justify-content-center display-3">
+    <a href="https://github.com/fabricioGuac" target="_blank"><FaGithub  /></a>
+    <a href="https://www.linkedin.com/in/fabricio-guacuto" target="_blank"><FaLinkedin  /></a>
+    <a href="https://stackoverflow.com/questions" target="_blank"><FaStackOverflow  /></a>
+</footer>
     )
 }
--- src/components/Poject.jsx
+++ src/components/Poject.jsx
@@ -1,11 +1,13 @@
+import { FaGithub  } from "react-icons/fa"
+
 export default function Project(props){
 
     return (
     <div className="col-md-6">
 
         <img src={props.img} alt={props.name} />
-        <a href={props.link} target="_blank"><h3>{props.name}</h3></a>
-        <a href={props.repo} target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/100px-GitHub_Invertocat_Logo.svg.png" alt="Github logo" /></a>
+        <a href={props.link} target="_blank"><FaGithub className="display-3"/><h3>{props.name}</h3></a>
+        <a href={props.repo} target="_blank"></a>
 
     </div>
 )
