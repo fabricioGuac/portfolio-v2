3717b2b217a30c4ae5b9668337b26cf406bcd7fb
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-07-20 19:47:02 -0700

    adjusts icon separation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,11 @@ import { FaGithub, FaLinkedin, FaStackOverflow  } from "react-icons/fa"
 export default function Footer() {
     return (
 <footer className="d-flex justify-content-center display-3">
+    <div className="icons">
     <a href="https://github.com/fabricioGuac" target="_blank"><FaGithub  /></a>
     <a href="https://www.linkedin.com/in/fabricio-guacuto" target="_blank"><FaLinkedin  /></a>
     <a href="https://stackoverflow.com/questions" target="_blank"><FaStackOverflow  /></a>
+    </div>
 </footer>
     )
 }
